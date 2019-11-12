60aa9810171610f42c2c36ace3597a46139e7b0a
novwaul <123dlswp@kaist.ac.kr>
2019-11-12 13:59:47 +0900

    add more test cases

--- CS204/PA2/testSolution.py
+++ CS204/PA2/testSolution.py
@@ -7,5 +7,21 @@ class testSolution(unittest.TestCase):
         result = solution.Main(stringForTest)
         self.assertEqual([6,8,3,4,0], result)
 
+    def testValidNoneDupString(self):
+        stringForTest = "ADBEC"
+        result = solution.Main(stringForTest)
+        self.assertEqual([5,8,6,9,7], result)
+    
+    def testValidDoubleDupString(self):
+        stringForTest = "BBCCEBB"
+        result = solution.Main(stringForTest)
+        self.assertEqual([6,1,2,7,9,6,1], result)
+    
+    def testInvalidString(self):
+        stringForTest = "ADCBB"
+        result = solution.Main(stringForTest)
+        self.assertEqual(-1, result)
+        
+
 if __name__ == '__main__':
     unittest.main()
