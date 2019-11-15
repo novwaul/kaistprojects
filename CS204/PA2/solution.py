5a71ee6507e1d7464ae877f923ac3a0e71f9a96a
novwaul <123dlswp@kaist.ac.kr>
2019-11-15 20:10:03 +0900

    Make vertex class and vertexFactory class.
    Also add vertexFactory check cases in testSolution.py

--- CS204/PA2/solution.py
+++ CS204/PA2/solution.py
@@ -1,4 +1,110 @@
 #TODO use OOP to reduce duplication
+
+class vertex:
+    def __init__(self, id, oppositeSideId, neighborsId):
+        self.id = id
+        self.oppositeSideId = oppositeSideId
+        self.neighborsId = neighborsId
+    
+    def __eq__(self, other):
+        return self.id == other.id and self.oppositeSideId == other.oppositeSideId and self.neighborsId == other.neighborsId
+
+    def getId(self):
+        return self.id
+
+    def getOppositeSideId(self):
+        return self.oppositeSideId
+
+    def getNeighborsId(self):
+        return self.neighborsId
+
+
+class vertexFactory:
+    #CAUTION: defaultVertexNum must be even.
+    def __init__ (self, defaultVertexName = 'A', defaultVertexId = 0, defaultVertexNum = 10):
+        self.defaultVertexName = defaultVertexName
+        self.defaultVertexNum = defaultVertexNum
+        self.defaultOuterVertexId = defaultVertexId
+        self.defaultInnerVertexId = self.calculateDefaultInnerVertexId()
+    
+    def calculateDefaultInnerVertexId(self):
+        return (self.defaultVertexNum + self.defaultOuterVertexId) // 2
+
+    def makeInnerVertex(self, vertexName):
+        id = self.calculateInnerVertexId(vertexName)
+        neighborsId = self.calculateInnerVertexNeighborsId(vertexName)
+        oppositeSideId = self.calculateOuterVertexId(vertexName)
+        return vertex(id, oppositeSideId, neighborsId)
+
+    def calculateInnerVertexId(self, vertexName):
+        vertexOffset = self.calculateVertexOffset(vertexName)
+        return self.defaultInnerVertexId + vertexOffset
+
+    def calculateVertexOffset(self, vertexName):
+        return ord(vertexName) - ord(self.defaultVertexName)
+
+    def calculateInnerVertexNeighborsId(self, vertexName):
+        defaultInnerVertexNeighborsId = self.calculateDefaultInnerVertexNeighborsId()
+        vertexOffset = self.calculateVertexOffset(vertexName)
+        newInnerVertexNeighborsId = []
+        for e in defaultInnerVertexNeighborsId:
+            result = self.addInnerVertexOffsetWithModOperation(e, vertexOffset)
+            newInnerVertexNeighborsId.append(result)
+        return newInnerVertexNeighborsId
+
+    def calculateDefaultInnerVertexNeighborsId(self):
+        neighborsId = []    
+        neighborIdWithSmallerOne = self.getMiddleInnerVertexId()
+        neighborIdwithBiggerOne = self.getNextId(neighborIdWithSmallerOne)
+        neighborsId.append(neighborIdWithSmallerOne)
+        neighborsId.append(neighborIdwithBiggerOne)
+        return neighborsId
+
+    def getMiddleInnerVertexId(self):
+        return self.defaultInnerVertexId + self.defaultVertexNum // 4
+
+    def getNextId(self, id):
+        return id + 1
+
+    def addInnerVertexOffsetWithModOperation(self, value, vertexOffset):
+        innerVertexNum = self.defaultVertexNum // 2
+        return innerVertexNum + (value + vertexOffset) % innerVertexNum
+
+    def makeOuterVertex(self, vertexName):
+        id = self.calculateOuterVertexId(vertexName)
+        neighborsId = self.calculateOuterVertexNeighborsId(vertexName)
+        oppositeSideId = self.calculateInnerVertexId(vertexName)
+        return vertex(id, oppositeSideId, neighborsId)
+
+    def calculateOuterVertexId(self, vertexName):
+        vertexOffset = self.calculateVertexOffset(vertexName)
+        return self.defaultOuterVertexId + vertexOffset
+    
+    def calculateOuterVertexNeighborsId(self, vertexName):
+        defaultOuterVertexNeighborsId = self.calculateDefaultOuterVertexNeighborsId()
+        vertexOffset = self.calculateVertexOffset(vertexName)
+        newOuterVertexNeighborsId = []
+        for e in defaultOuterVertexNeighborsId:
+            result = self.addOuterVertexOffsetWithModOperation(e, vertexOffset)
+            newOuterVertexNeighborsId.append(result)
+        return newOuterVertexNeighborsId
+
+    def calculateDefaultOuterVertexNeighborsId(self):
+        neighborsId = []    
+        neighborIdWithSmallerOne = self.getLastOuterVertexId()
+        neighborIdwithBiggerOne = self.getNextId(self.defaultOuterVertexId)
+        neighborsId.append(neighborIdWithSmallerOne)
+        neighborsId.append(neighborIdwithBiggerOne)
+        return neighborsId
+
+    def getLastOuterVertexId(self):
+        return self.defaultOuterVertexId + self.defaultVertexNum // 2 - 1
+
+    def addOuterVertexOffsetWithModOperation(self, value, vertexOffset):
+        outerVertexNum = self.defaultVertexNum // 2
+        return (value + vertexOffset) % outerVertexNum
+
+#TODO
 def FindVertexPath(solution, input):
     if(len(input) == 0):
         return True
--- CS204/PA2/testSolution.py
+++ CS204/PA2/testSolution.py
@@ -2,6 +2,20 @@ import unittest
 import solution
 
 class testSolution(unittest.TestCase):
+    def testMakeInnerVertex(self):
+        testVertexName = 'B'
+        vertexFactory = solution.vertexFactory()
+        vertexForTest = vertexFactory.makeInnerVertex(testVertexName)
+        vertexForAnswer = solution.vertex(6, 1, [8, 9])
+        self.assertEqual(True, vertexForAnswer == vertexForTest)
+
+    def testMakeOuterVertex(self):
+        testVertexName = 'B'
+        vertexFactory = solution.vertexFactory()
+        vertexForTest = vertexFactory.makeOuterVertex(testVertexName)
+        vertexForAnswer = solution.vertex(1, 6, [0, 2])
+        self.assertEqual(True, vertexForAnswer == vertexForTest)
+
     def testValidDuplicateString(self):
         stringForTest = "BDDEA"
         result = solution.Main(stringForTest)
